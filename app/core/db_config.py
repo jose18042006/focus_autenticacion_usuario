0dacf8ae709569838102e33891084f816d3ea63d
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-05-14 10:17:59 -0400

    tabla creada, config lista

--- app/core/db_config.py
+++ app/core/db_config.py
@@ -3,7 +3,6 @@ from advanced_alchemy.extensions.litestar import SQLAlchemyPlugin, SQLAlchemyAsy
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL", 
-    "postgresql+asyncpg://neondb_owner:npg_zWwlKLbHC7o1@ep-polished-wave-aq8tm0sf-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 )
 db_config = SQLAlchemyAsyncConfig(
     connection_string= DATABASE_URL
