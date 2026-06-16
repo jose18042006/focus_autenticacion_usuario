fd4b9b23522cda1fc380d9a67dc27200cceca9c3
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-15 23:17:57 -0400

    hotfix create tables auto

--- app/core/db_config.py
+++ app/core/db_config.py
@@ -1,11 +1,15 @@
 import os
 from advanced_alchemy.extensions.litestar import SQLAlchemyPlugin, SQLAlchemyAsyncConfig
+from app.models.base import Base    
+from app.models.user import UserModel
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL", 
 )
 db_config = SQLAlchemyAsyncConfig(
-    connection_string= DATABASE_URL
+    connection_string= DATABASE_URL,
+    create_all=True,
+    metadata=Base.metadata  
 )
 
 db_plugin = SQLAlchemyPlugin(config=db_config)
